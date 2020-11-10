db6c27833b0276e0c9a9c72c1f7a8a3630e782f5
FotieMConstant <moghommahieconstant@gmail.com>
2020-11-10 18:30:10 +0100

    done packaging with vue-sfc

---
+++ sfc-package/vue-codesandbox/dev/serve.vue
@@ -0,0 +1,21 @@
+<script>
+import Vue from "vue";
+import VueCodesandbox from "@/vue-codesandbox.vue";
+
+export default Vue.extend({
+  name: "ServeDev",
+  components: {
+    VueCodesandbox,
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-codesandbox
+      codeUrl="https://codesandbox.io/s/vue-vue?autoresize=1&fontsize=14&hidenavigation=1&theme=dark"
+      width="1000"
+      height="1000"
+    />
+  </div>
+</template>
---
+++ sfc-package/vue-codesandbox/src/vue-codesandbox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="codesandbox">
+    <embed type="text/html" :src="codeUrl" :width="width" :height="height" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueCodesandbox",
+  props: {
+    codeUrl: String,
+    width: Number,
+    height: Number,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
+
